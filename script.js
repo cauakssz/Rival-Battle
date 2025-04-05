34fca451c17e1827e1f61517dcf6a59c4ac0c44d
Cauã de Souza <caua19922@gmail.com>
2025-04-05 00:53:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-function mostrarTela(id) {
-    document
-        .querySelectorAll(".tela")
-        .forEach((tela) => tela.classList.remove("ativa"));
-    document.getElementById(id).classList.add("ativa");
+const btn = document.querySelector("#btn");
+const audioId = "myAudio"; // ID do elemento de áudio
+const telaId = "tela1"; // ID da tela que você quer mostrar
+const telaId2 = "tela2"; // ID da tela que você quer que pare a musica
+const audio = document.getElementById(audioId); // Obtém o elemento de áudio
+
+const btn2 = document.querySelector("#btn2");
+    const audioId2 = "click";
+const telaId3 = "tela2" ; // ID da tela que você quer mostrar
+  const telaId4 = "tela0" // ID da tela que você quer que pare a musica
+  const audio2 = document.getElementById(audioId2); // Obtém o elemento de áudio
+
+
+
+
+function myAudio(id) {
+  const audio = document.getElementById(id);
+  if (audio) {
+    audio.loop = true;
+    // Define o loop como verdadeiro
+    audio.play();
+  } else {
+    console.error("Audio element with id '" + id + "' not found.");
+  }
 }
+
+function click(id) {
+    const audio2 = document.getElementById(id);
+    if (audio2) {
+        audio2.currentTime = 0.4677
+      audio2.play();
+    } else {
+      console.error("Audio element with id '" + id + "' not found.");
+    }
+  }
+
+function mostrarTela(id) {
+  document
+    .querySelectorAll(".tela")
+    .forEach((tela) => tela.classList.remove("ativa"));
+  document.getElementById(id).classList.add("ativa");
+    if (id === telaId2) {
+        audio.pause();
+        audio.currentTime = 0; // Reseta o áudio para o início
+    }
+}
+
+// Adiciona o evento de clique ao botão
+btn.addEventListener("click", function() {
+  myAudio(audioId);
+  mostrarTela(telaId);
+  
+});
+
+btn2.addEventListener("click", function() {
+    click(audioId2);
+    mostrarTela(tela0);
+    
+  });
+  
 
 function escolher(choice) {
     let pokemons = {
@@ -19,25 +73,24 @@ const fraquezas = {
      };
   
 //escolhaJogador = prompt("Escolha seu Pókemon: 1-Bulbasauro, 2-Charmander ou 3-Squirtle");//
- const escolhaPc = Math.floor(Math.random() * 3) + 1;
-  let rival = pokemons[escolhaPc];
+    const escolhaPc = Math.floor(Math.random() * 3) + 1;
+    let rival = pokemons[escolhaPc];
     let escolhaJogador = pokemons[choice];
   
-  let resultado = `O Rival escolheu: ${rival}\n`;
+    let resultado = `O Rival escolheu: ${rival}\n`;
   
-    if (
-        fraquezas[escolhaJogador] &&
-        fraquezas[escolhaJogador].includes(rival)
-    ) {
-        resultado += `\nVOCÊ PERDEU!\nO ${rival} do Rival foi super efetivo contra o seu${escolhaJogador}!\nRival: Hahahaha isso é tudo que tem? Levante-se e lute!!`;
+    if (fraquezas[escolhaJogador] && fraquezas[escolhaJogador].includes(rival)) {
+      resultado += `\nVOCÊ PERDEU!\nO ${rival} do Rival foi super efetivo contra o seu ${escolhaJogador}!\nRival: Hahahaha isso é tudo que tem? Levante-se e lute!!`;
       
     } else if (fraquezas[rival] && fraquezas[rival].includes(escolhaJogador)) {
-        resultado += `\nVOCÊ VENCEU!\nO seu ${escolhaJogador} foi super efetivo contra o ${rival} do Rival!\nRival: Não fique se achando porque teve sorte, vamos novamente!`;
+      resultado += `\nVOCÊ VENCEU!\nO seu ${escolhaJogador} foi super efetivo contra o ${rival} do Rival!\nRival: Não fique se achando porque teve sorte, vamos novamente!`;
       
     } else {
-        resultado += "\EMPATOU!!!\nAmbos os Pókemons estão fora de combate!\nRival: Grrr! Agora vou batalhar serio!!.";
+      resultado += "\nEMPATOU!!!\nAmbos os Pókemons estão fora de combate!\nRival: Grrr! Agora vou batalhar serio!!.";
+     
     }
-  
-document.getElementById("resultado").innerText = resultado;
-mostrarTela("tela2"); }
-    
+
+
+    document.getElementById("resultado").innerText = resultado;
+    mostrarTela("tela2"); }
+
